a6184746d3b0eff6cdd03eeb0173218e8421ac22
Leeya Davis <leeya463@gmail.com>
2019-03-21 16:23:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+    collection
+  else
+    "No block given!"
+  end
 end
+
+my_each(["Leeya", "Jason", "Derek"]) do |element|
+  "#{element}".reverse
+end
+
+
+
+
+#use 'yield' and 'while' loop to pass each element of collection to a block
+#
